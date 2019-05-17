362d3b76ea4f37e06ddd78b8a66391c765112a4a
Jesse Taavola <jessetaavola@gmail.com>
2019-05-17 17:14:02 -0500

    Check input format; Handle 'N.A.' item name

--- dpci_lookup.py
+++ dpci_lookup.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 # Lookup DPCI using BrickSeek Target Inventory Checker
 # 
@@ -9,6 +10,11 @@ import requests
 # input: dpci - string in format 123-45-6789
 # output: Item name on success, None on failure
 def get_name(dpci):
+
+  # check input format using reg ex
+  if not re.match(r'^\d{3}-\d{2}-\d{4}$', dpci):
+    return None
+
   try:
     res = requests.get('https://brickseek.com/target-inventory-checker/?sku=' + dpci)
   except requests.exceptions.RequestException as e:
@@ -17,7 +23,7 @@ def get_name(dpci):
 
   data = res.text
   # Use Beautiful Soup to parse the HTML for 'title'
-  soup = BeautifulSoup(data, features="html.parser")
+  soup = BeautifulSoup(data, features='html.parser')
 
   item_name = soup.title.string
 
@@ -26,16 +32,20 @@ def get_name(dpci):
   if item_name.endswith(extra_name):
     item_name = item_name[:-(len(extra_name))]
   else:
+    # Item name format not recognized
+    return None
+
+  if item_name == 'N.A.':
     # Item not found
-    item_name = None
+    return None
 
   return item_name
 
 # Demo for DPCI lookup
-if __name__ == "__main__":
-  dpci = input("Enter DPCI: ")
+if __name__ == '__main__':
+  dpci = input('Enter DPCI: ')
   name = get_name(dpci)
   if name:
     print(name)
   else:
-    print("Error retrieving item name.")
+    print('Error retrieving item name.')
